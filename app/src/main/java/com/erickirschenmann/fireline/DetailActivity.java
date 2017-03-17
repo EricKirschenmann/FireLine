f1c6be6bc812e5483ff982669179adde081b8cd5
Eric Kirschenmann <eric.kirschenmann@gmail.com>
2017-03-16 17:24:42 -0700

    Set up bare bones Detail Activity with back button

--- app/src/main/java/com/erickirschenmann/fireline/DetailActivity.java
+++ app/src/main/java/com/erickirschenmann/fireline/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.erickirschenmann.fireline;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class DetailActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_detail);
+
+    if (getActionBar() != null) {
+      getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
