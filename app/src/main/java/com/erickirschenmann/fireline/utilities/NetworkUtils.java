bd4786c24bd657106a61f41b9deb0bacc33f0c56
Eric Kirschenmann <eric.kirschenmann@gmail.com>
2017-03-23 13:38:17 -0700

    use resources instead of hard coding preference strings and booleans

--- app/src/main/java/com/erickirschenmann/fireline/utilities/NetworkUtils.java
+++ app/src/main/java/com/erickirschenmann/fireline/utilities/NetworkUtils.java
@@ -2,10 +2,11 @@ package com.erickirschenmann.fireline.utilities;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import com.erickirschenmann.fireline.MainActivity;
+import com.erickirschenmann.fireline.R;
 import com.erickirschenmann.fireline.data.FirelineTestJson;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,7 +18,6 @@ import java.util.Scanner;
 /** Created by eric on 3/3/17. */
 public class NetworkUtils {
 
-  private static final boolean DEBUG = true;
   private static final String TAG = NetworkUtils.class.getSimpleName();
   private static final String FIRELINE_JSON_URL = "http://fireline.ventura.org/data/fireline.json";
 
@@ -47,10 +47,14 @@ public class NetworkUtils {
    * @throws IOException Related to network and stream reading
    */
   public static String getResponseFromHttpUrl(URL url, Context context) throws IOException {
-
+    // use shared preferences to get user's settings about debug data
     SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+    // get the values from resources
+    Resources resources = context.getResources();
+    String debugKey = resources.getString(R.string.pref_show_debug_key);
+    boolean debugDefault = resources.getBoolean(R.bool.pref_show_debug_default);
 
-    if (sharedPreferences.getBoolean("show_debug", false)) {
+    if (sharedPreferences.getBoolean(debugKey, debugDefault)) {
       // instead of constantly downloading from their server, use test data
       return FirelineTestJson.getFIRELINE_TEST_JSON();
     } else {
