14435ba0e8007bc55235c491113a3a1bee0fc239
Eric Kirschenmann <eric.kirschenmann@gmail.com>
2017-03-24 23:04:15 -0700

    setup database contract which defines the columns and table name

---
+++ app/src/main/java/com/erickirschenmann/fireline/data/IncidentContract.java
@@ -0,0 +1,21 @@
+package com.erickirschenmann.fireline.data;
+
+import android.provider.BaseColumns;
+
+/** Created by eric on 3/24/17. */
+public class IncidentContract {
+  public static final class IncidentEntry implements BaseColumns {
+    public static final String TABLE_NAME = "incident";
+    public static final String COLUMN_ADDRESS = "address";
+    public static final String COLUMN_BLOCK = "block";
+    public static final String COLUMN_CITY = "city";
+    public static final String COLUMN_COMMENT = "comment";
+    public static final String COLUMN_INCIDENT_NUMBER = "incidentNumber";
+    public static final String COLUMN_INCIDENT_TYPE = "incidentType";
+    public static final String COLUMN_LATITUDE = "latitude";
+    public static final String COLUMN_LONGITUDE = "longitude";
+    public static final String COLUMN_RESPONSE_DATE = "responseDate";
+    public static final String COLUMN_STATUS = "status";
+    public static final String COLUMN_UNITS = "units";
+  }
+}
