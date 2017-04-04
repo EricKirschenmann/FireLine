eb55ccfc43bf5446cc313588bc29b46be8ed4c16
Eric Kirschenmann <eric.kirschenmann@gmail.com>
2017-04-03 23:21:24 -0700

    Following SQLite examples to try and get it functioning then will change it once working

--- app/src/main/java/com/erickirschenmann/fireline/data/WeatherDbHelper.java
+++ app/src/main/java/com/erickirschenmann/fireline/data/WeatherDbHelper.java
@@ -1,23 +1,38 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.erickirschenmann.fireline.data;
 
-/** Created by eric on 3/24/17. */
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.erickirschenmann.fireline.data.WeatherContract.WeatherEntry;
 
-/** Manages a local database for incident data. */
-// COMPLETED (11) Extend SQLiteOpenHelper from WeatherDbHelper
+/**
+ * Created by eric on 3/24/17.
+ *
+ * <p>Manages a local database for incident data.
+ */
 public class WeatherDbHelper extends SQLiteOpenHelper {
 
-  // COMPLETED (12) Create a public static final String called DATABASE_NAME with value "weather.db"
   /*
    * This is the name of our database. Database names should be descriptive and end with the
    * .db extension.
    */
   public static final String DATABASE_NAME = "weather.db";
 
-  // COMPLETED (13) Create a private static final int called DATABASE_VERSION and set it to 1
   /*
    * If you change the database schema, you must increment the database version or the onUpgrade
    * method will not be called.
@@ -31,12 +46,10 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
    */
   public static final int DATABASE_VERSION = 1;
 
-  // COMPLETED (14) Create a constructor that accepts a context and call through to the superclass constructor
   public WeatherDbHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
   }
 
-  // COMPLETED (15) Override onCreate and create the weather table from within it
   /**
    * Called when the database is created for the first time. This is where the creation of tables
    * and the initial population of the tables should happen.
@@ -87,7 +100,6 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
     sqLiteDatabase.execSQL(SQL_CREATE_WEATHER_TABLE);
   }
 
-  // COMPLETED (16) Override onUpgrade, but don't do anything within it yet
   /**
    * This database is only a cache for online data, so its upgrade policy is simply to discard the
    * data and call through to onCreate to recreate the table. Note that this only fires if you
